9a59706e0245c04264f9ada1d965148be0c9dbe1
kimsy0712 <worldkim0712@naver.com>
2025-07-28 12:19:28 +0900

    feat: 즐겨찾기 테스트용 app.js 구현

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider } from './src/context/AuthContext'; // 임시 파일
+import { BookmarkProvider } from './src/context/BookmarkContext';
+import BookmarkScreen from './src/screens/main/BookmarkScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>initial</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <BookmarkProvider>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Bookmarks">
+              <Stack.Screen 
+                name="Bookmarks" 
+                component={BookmarkScreen} 
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </BookmarkProvider>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
